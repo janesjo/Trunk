f945fad587535ddc2db44f246d029116a06055df
Jonathan Janesjo <jonathan.janesjo@outlook.com>
2018-03-05 22:58:04 +0100

    Added MacMetalTestGame and iOS Test App
    Added MacMetalTestGame and iOS Test App

---
+++ MacMetalTestGame/MacMetalTestGame/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace MacMetalTestGame
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
